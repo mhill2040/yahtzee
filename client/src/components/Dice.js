660f666baa4307859225b065e724c045827a50c1
Melissa Hill <mhill2040@gmail.com>
2018-03-26 12:22:33 -0600

    Day 1

---
+++ client/src/components/Dice.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+import d1 from '../images/d1.png';
+import d2 from '../images/d2.png';
+import d3 from '../images/d3.png';
+import d4 from '../images/d4.png';
+import d5 from '../images/d5.png';
+import d6 from '../images/d6.png';
+
+const styles = {
+  dice: { marginLeft: '20px', cursor: 'pointer' },
+  selected: { borderBottom: 'solid 2px blue' }
+}
+
+const images = { d1, d2, d3, d4, d5, d6 };
+
+const Dice = ({ value, index, kept, toggleKept }) => (
+   <Grid.Column textAlign="center" width={3}>
+     <Image
+       style={ kept ? { ...styles.dice, ...styles.selected } : styles.dice }
+       src={images[`d${value}`]}
+       alt={`dice value ${value}`}
+       onClick={ () => toggleKept(index) }
+     />
+   </Grid.Column>
+)
+
+export default Dice;
