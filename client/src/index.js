43f445c2dc2fdeaf1acf1c742debf57448f22ddc
Melissa Hill <mhill2040@gmail.com>
2018-03-27 13:30:42 -0600

    day 2

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import store from './store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
